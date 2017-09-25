84156de4b2725221bbc5314d9ffb40a37388c9d2
Mattia Manzati <mattia.manzati@gmail.com>
2017-09-25 22:49:37 +0200

    do not rely on connectReduxDevtools

--- src/run.js
+++ src/run.js
@@ -1,10 +1,39 @@
 import { transform } from "babel-standalone";
-import { onAction, onPatch, onSnapshot, getSnapshot } from "mobx-state-tree";
-import { connectReduxDevtools } from "mobx-state-tree/middleware/redux"
+import { onAction, onPatch, onSnapshot, getSnapshot, applySnapshot } from "mobx-state-tree";
 const MST = require("mobx-state-tree");
 const MobX = require("mobx");
 const React = require("react");
 const MobXReact = require("mobx-react");
+
+function connectReduxDevtools(remoteDevDep, model) {
+  // Connect to the monitor
+  const remotedev = remoteDevDep.connectViaExtension()
+  let applyingSnapshot = false
+
+  // Subscribe to change state (if need more than just logging)
+  remotedev.subscribe(message => {
+      // Helper when only time travelling needed
+      const state = remoteDevDep.extractState(message)
+      if (state) {
+          applyingSnapshot = true
+          applySnapshot(model, state)
+          applyingSnapshot = false
+      }
+  })
+
+  // Send changes to the remote monitor
+  onAction(
+      model,
+      action => {
+          if (applyingSnapshot) return
+          const copy = {}
+          copy.type = action.name
+          if (action.args) action.args.forEach((value, index) => (copy[index] = value))
+          remotedev.send(copy, getSnapshot(model))
+      },
+      true
+  )
+}
 
 export function transpile(code) {
   let transformed = "";
